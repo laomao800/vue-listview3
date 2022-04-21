9ad75553ef80af88256fabca4fe0f722cde5e14f
laomao800 <laomao800@gmail.com>
2022-04-21 15:36:29 +0800

    add eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracket: false,
+  arrowParens: 'always',
+  proseWrap: 'never',
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'ignore',
+}
