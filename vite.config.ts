037cb80331d4dc1f41fc3a47c59b1b88a443a720
laomao800 <laomao800@gmail.com>
2022-07-07 16:38:29 +0800

    移除 unplugin-xxx

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,51 @@
-import path from 'path'
+import path, { resolve } from 'path'
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import DefineOptions from 'unplugin-vue-define-options/vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { visualizer } from 'rollup-plugin-visualizer'
+import pkg from './package.json'
+
+const depNames = Object.keys(pkg.dependencies)
+depNames.push('vue')
+const depNamesReg = new RegExp(`^(${depNames.join('|')})`)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    DefineOptions(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname),
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ command }) => {
+  const plugins = [vue(), vueJsx(), DefineOptions()]
+  let publicDir: UserConfig['publicDir'] = 'public'
+
+  if (command === 'build') {
+    plugins.push(visualizer())
+    publicDir = false
+  }
+
+  return {
+    publicDir,
+    plugins,
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname),
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
+    build: {
+      lib: {
+        entry: resolve('./src/index.ts'),
+        name: 'listview',
+        fileName: 'index',
+      },
+      rollupOptions: {
+        external: (id) => depNamesReg.test(id),
+        output: {
+          globals: {
+            'lodash-es': '_',
+            'element-plus': 'ElementPlus',
+            axios: 'axios',
+          },
+        },
+      },
+    },
+  }
 })
